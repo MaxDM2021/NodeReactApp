62582cd26f0999299bd89325d4a1f4d0c3fac5f4
Maksym Dmitriiev <maximdm22@gmail.com>
2023-09-11 22:53:10 +0100

    1

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('8888 Hello World!');
+});
+
+app.listen(4444, (err) => {
+    if(err) {
+        return console.log(err);
+    }
+
+console.log('Server OK');
+
+});
